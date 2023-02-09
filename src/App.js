a49a48353c7d3011fcbc0f3b793ecbda0fc883af
Esteban Pajares <jehupajares@gmail.com>
2023-02-09 00:16:03 -0500

    Add events to user component and app

--- src/App.js
+++ src/App.js
@@ -12,7 +12,12 @@ function Header({children}){
 }
 
 function App() {
-  const name = 'Esteban'
+
+function handleSubmit(event){
+  event.preventDefault() //Para no recargar el navegador
+  console.log('No recarga el navegador')
+}
+
 return (
     <div className="App">
       <Header>
@@ -27,6 +32,13 @@ return (
       name = 'Esteban'
       avatar = 'https://www.elcolombiano.com/binrepository/580x872/0c10/580d365/none/11101/GBFJ/capitan-america_37326520_20210310181935.jpg'
       />
+
+      <form action='' onSubmit={handleSubmit}>
+        <input type="text" />
+        <input type="text" />
+        <input type="submit" />
+      </form>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello World</h1>
--- src/user.js
+++ src/user.js
@@ -1,8 +1,18 @@
 // Creamos la funcion
+
+//Eventos en JS
+//Element.addEventListener('click', ()=>{})
+
+
 function User({name, avatar}){
     const newName = `${name} es el Capitan América`
+
+    function onClick(){
+        console.log(`hola ${name} Este es el resultado del evento Click`)
+    }
+
     return (
-        <div className="User">
+        <div className="User" onClick={onClick}>
             <img src={avatar} alt={name} title = {name} />
             <p>{newName}</p>
         </div>
