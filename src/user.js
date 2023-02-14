833cb123a87fbaed26466effa1bc4c1eaa84c186
Esteban Pajares <jehupajares@gmail.com>
2023-02-13 23:56:18 -0500

    Add useState to project

--- src/user.js
+++ src/user.js
@@ -1,3 +1,6 @@
+// Funcion para el estado
+import { useState } from "react"
+
 // Creamos la funcion
 
 //Eventos en JS
@@ -5,16 +8,21 @@
 
 
 function User({name, avatar}){
-    const newName = `${name} es el Capitan América`
+    const [calculatedName, setCalculatedName] = useState(name)
+    const [counter, setCounter] = useState(0)
+
+    const newName = `${calculatedName}, quien será tu maestro de informática`
 
     function onClick(){
+        setCalculatedName(`Capitan América`)
+        setCounter( counter + 1)
         console.log(`hola ${name} Este es el resultado del evento Click`)
     }
 
     return (
         <div className="User" onClick={onClick}>
             <img src={avatar} alt={name} title = {name} />
-            <p>{newName}</p>
+            <p>Evento click by {newName} por {counter} veces</p>
         </div>
     )
 }
