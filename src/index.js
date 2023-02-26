8d8842dfd8cf236f4146db5a79d7f311a83ad2ec
Esteban Pajares <jehupajares@gmail.com>
2023-02-26 00:19:10 -0500

    Add global-tyles component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStylesStyled from './components/global-styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <>
+    <GlobalStylesStyled/>
     <App />
+    </>
   </React.StrictMode>
 );
 
